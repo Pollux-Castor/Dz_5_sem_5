582388e889a6120a405de01824a153a02945d0dd
Maxim_1623 <sabatag777@vivaldi.net>
2023-04-28 14:15:07 +0300

    Tusk 36 added

--- Program.cs
+++ Program.cs
@@ -2,30 +2,49 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] Massive =new int[10];
+// int[] Massive =new int[10];
 
-int hetNum=0;
+// int hetNum=0;
 
-for(int i=0; i<Massive.Length; i++)
+// for(int i=0; i<Massive.Length; i++)
 
-{
-Massive [i] =new Random().Next(100,1000);
+// {
+// Massive [i] =new Random().Next(100,1000);
 
-if (Massive[i]%2==0)
+// if (Massive[i]%2==0)
 
-            {
-            hetNum=hetNum+1;
-            }
+//             {
+//             hetNum=hetNum+1;
+//             }
 
-}
-Console.WriteLine(String.Join(",",Massive));
-Console.WriteLine(hetNum);
+// }
+// Console.WriteLine(String.Join(",",Massive));
+// Console.WriteLine(hetNum);
 
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] Massive =new int[4];
+
+int NehetNum=0;
+
+for(int i=0; i<Massive.Length; i++)
+
+{
+Massive [i] =new Random().Next(-99,100);
+
+if (Massive[i]%2 !=0)
+
+            {
+            NehetNum+=Massive[i]; //NehetNum=NehetNum+Massive[i]
+            }
+
+}
+Console.WriteLine(String.Join(",",Massive));
+Console.WriteLine(NehetNum);
 
 
 
